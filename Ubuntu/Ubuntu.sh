e017b48a1b110c2186e5b1380f508fb6eee55b12
Noisk8 <juansk8jaramillo@gmail.com>
2018-09-27 00:19:21 -0500

    37a

--- Ubuntu/Ubuntu.sh
+++ Ubuntu/Ubuntu.sh
@@ -63,23 +63,27 @@ sudo apt-get install g++
 sudo apt-get install dh-autoreconf
 
 echo Descargando código fuente de DIN
+  GNU nano 2.5.3                                                    Archivo: din37.sh                                                                                                                 
 
 wget https://archive.org/download/dinisnoise_source_code/din-37a.tar.gz
 
-tar -xvf din-37a/
+tar -xvf din-37a.tar.gz
 
 echo Configurando DIN
 
-cd din-37a/;  autoreconf -fvi
+cd din-37a/ 
+
+
+autoreconf -fvi
 
 #Si quieres usarlo con Jack descomenta la siguiente linea
 
-#./configure CXXFLAGS="-O3 -D__UNIX_JACK__" CFLAGS=-O3
+./configure CXXFLAGS="-O3 -D__UNIX_JACK__" CFLAGS=-O3
 
-./configure CXXFLAGS="-O3 -D__LINUX_ALSA__" CFLAGS=-O3
+#./configure CXXFLAGS="-O3 -D__LINUX_ALSA__" CFLAGS=-O3
 
 make
 
 sudo make install
 
-echo Es recomendable que reinicies tu ordenador y al retornar Ejecuta din en la terminal y disfruta!
+echo Es recomendable que reinicies tu ordenador y al retornar Ejecuta din en la terminal y disfruta
