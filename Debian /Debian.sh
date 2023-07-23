36cbea192e48853f26787bed4059ee5f8a864b22
Noisk8 <juansk8jaramillo@gmail.com>
2023-07-23 16:50:58 -0500

    Update Debian.sh

--- Debian /Debian.sh
+++ Debian /Debian.sh
@@ -8,45 +8,45 @@ echo Instalando boost
 
 sudo apt-get update
 
-sudo apt-get install libboost-all-dev
+sudo apt-get install libboost-all-dev -y
 
 echo Instalando openGL
 
 sudo apt-get update
 
-sudo apt-get install freeglut3
+sudo apt-get install freeglut3 -y
 
-sudo apt-get install freeglut3-dev
+sudo apt-get install freeglut3-dev -y
 
-sudo apt-get install binutils-gold
+sudo apt-get install binutils-gold -y
 
-sudo apt-get install libglew-dev
+sudo apt-get install libglew-dev -y
 
-sudo apt-get install mesa-common-dev
+sudo apt-get install mesa-common-dev -y
 
-sudo apt-get install libglew1.5-dev libglm-dev
+sudo apt-get install libglew1.5-dev libglm-dev -y
 
 echo Instalando sdl
 
-sudo apt-get install libsdl1.2-dev
+sudo apt-get install libsdl1.2-dev -y
 
 echo Instalando tcl
 
-sudo apt-get install tcl8.5
+sudo apt-get install tcl8.5 -y
 
-sudo apt-get install tcl8.5-dev
+sudo apt-get install tcl8.5-dev -y
 
-sudo apt-get install tcl-dev
+sudo apt-get install tcl-dev -y 
 
 echo Instalando jack
 
-sudo apt-get install jack
+sudo apt-get install jack -y
 
-sudo apt-get install libjack-dev
+sudo apt-get install libjack-dev -y
 
-sudo apt-get install libjack-jackd2-dev
+sudo apt-get install libjack-jackd2-dev -y
 
-sudo apt-get install libjackd2
+sudo apt-get install libjackd2 -y
 
 echo Agregando el usuario al grupo audio
 
@@ -58,13 +58,13 @@ sudo apt --fix-broken install
 
 echo Instalando subversion
 
-sudo apt-get install subversion
+sudo apt-get install subversion -y
 
 echo Instalando autoreconf y g++
 
-sudo apt-get install g++
+sudo apt-get install g++ -y
 
-sudo apt-get install dh-autoreconf
+sudo apt-get install dh-autoreconf -y
 
 echo Descargando codigo fuente de Din
 
